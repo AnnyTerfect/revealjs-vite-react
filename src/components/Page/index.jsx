9971bb4a3ed916322c760fd1691492b0b102cf52
AnnyTerfect <1247470246@qq.com>
2023-07-17 19:36:50 +0800

    style: wider border bottom

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -4,7 +4,7 @@ function Page({ title, children, ...props }) {
   return (
     <section className="px-20 py-10 h-full" {...props}>
       {!!title && (
-        <h1 data-id="title" className="border-b-2 border-black pb-2 !mb-10">
+        <h1 data-id="title" className="border-b-4 border-black pb-2 !mb-10">
           {title}
         </h1>
       )}
