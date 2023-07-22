296b898cce1ffaedb05a6ee2831414955451187f
AnnyTerfect <1247470246@qq.com>
2023-07-22 23:49:40 +0800

    feat: complete decodehtml rules

--- src/components/Math/Equation/index.jsx
+++ src/components/Math/Equation/index.jsx
@@ -8,10 +8,17 @@ function decodeHtmlEntities(htmlString) {
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
     .replace(/&quot;/g, '"')
-    .replace(/&#039;/g, "'");
+    .replace(/&#039;/g, "'")
+    .replace(/&nbsp;/g, " ")
+    .replace(/&copy;/g, "©")
+    .replace(/&reg;/g, "®")
+    .replace(/&euro;/g, "€")
+    .replace(/<!--.*?-->/g, "")
+    .replace(/<\?.*?\?>/g, "")
+    .replace(/<!.*?>/g, "");
 }
 
-function Equation({ children }) {
+function Equation({ children, ...props }) {
   return window.MathJax.tex2svg ? (
     <div
       className="text-center my-2 mx-0"
@@ -22,6 +29,7 @@ function Equation({ children }) {
           )
         ).innerHTML,
       }}
+      {...props}
     />
   ) : (
     children
