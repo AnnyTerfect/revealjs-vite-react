a7d039189a1fffd47680afdea894757207eac9f7
AnnyTerfect <1247470246@qq.com>
2023-07-22 23:52:35 +0800

    feat: add utils

--- src/components/Math/Equation/index.jsx
+++ src/components/Math/Equation/index.jsx
@@ -2,22 +2,6 @@ import React from "react";
 import { renderToString } from "react-dom/server";
 import "mathjax-full/es5/tex-svg-full";
 
-function decodeHtmlEntities(htmlString) {
-  return htmlString
-    .replace(/&amp;/g, "&")
-    .replace(/&lt;/g, "<")
-    .replace(/&gt;/g, ">")
-    .replace(/&quot;/g, '"')
-    .replace(/&#039;/g, "'")
-    .replace(/&nbsp;/g, " ")
-    .replace(/&copy;/g, "©")
-    .replace(/&reg;/g, "®")
-    .replace(/&euro;/g, "€")
-    .replace(/<!--.*?-->/g, "")
-    .replace(/<\?.*?\?>/g, "")
-    .replace(/<!.*?>/g, "");
-}
-
 function Equation({ children, ...props }) {
   return window.MathJax.tex2svg ? (
     <div
